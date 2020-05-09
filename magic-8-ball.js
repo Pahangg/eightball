6a07dd59984977cb5ef2a9f805406bf9dd88b5d1
Paniz Ahang <pahang@ucvts.org>
2020-05-08 21:25:21 -0400

    done

--- magic-8-ball.js
+++ magic-8-ball.js
@@ -1,6 +1,24 @@
-var button = getElementById("button");
-var answer = getElementById("answer");
-
-button.addEventListener("click", function() {
-  alert("The button was clicked!");
-});
+function shake() {
+  //makes sure there is a question or at least some text in the textarea
+  if(document.getElementById("userQuestion").value == ''){
+    alert("Please Enter A Question!");
+  }
+  else {
+    var div=document.getElementById('answer');
+    //makes sure to clear whatever answer is in the space before generating the new one
+    div.innerHTML = "";
+    //choses a number between 1 and 10
+    var x = Math.random() * 10;
+    if (x<=5){
+      var div=document.getElementById('answer');
+      var textNode = document.createTextNode("Of Course");
+      var response='yes'
+    }
+    else {
+      var response='no'
+    }
+    //creates a new textnode with the response based off of the if statement
+    var textNode = document.createTextNode(response);
+    div.appendChild(textNode);
+  }
+}
